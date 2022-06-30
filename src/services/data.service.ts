57d2b8e807a6b72ab106bd92be6f0d9b77f5df2d
istromenko <iistromenko@gmail.com>
2022-06-30 14:26:11 +0300

    add header link

--- src/services/data.service.ts
+++ src/services/data.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from "@angular/core";
 import { Anime, AnimeLink } from "../main.interfaces";
 
 @Injectable({
-  providedIn: "root"
+  providedIn: "root",
 })
 export class DataService {
   constructor() { }
